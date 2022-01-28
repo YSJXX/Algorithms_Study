c1616a72b62f8454b69c754c4a2a7b45cce990d8
Mac_YSJ <424tjdwns@gmail.com>
2022-01-29 03:36:42 +0900

    solved

---
+++ January/13305.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+int main(){
+    int N;scanf("%d",&N);
+    long long dist[N];
+    long long minv=1000000001;
+    long long money=0;
+    long long ans=0;
+    for(int i=0;i<N-1;++i) scanf("%lld",&dist[i]);
+
+    for(int i=0;i<N-1;++i){
+        scanf("%lld",&money);
+        minv = minv < money ? minv : money;
+        ans += minv * dist[i];
+    }
+    printf("%lld\n",ans);
+    return 0;
+}
+
+// #include <stdio.h>
+// typedef struct _station{
+//     long long dist;
+//     long long money;
+// }Station;
+
+// int main(){
+//     int N;scanf("%d",&N);
+//     Station sta[N];
+
+//     long long ChargeDist = 0;
+//     long long minv=1000000001;
+//     long long next_sta=0;
+//     long long ans=0;
+
+//     for(long long i=0;i<N-1;++i) {
+//         scanf("%lld",&sta[i].dist);
+//     }
+//     for(long long i=0;i<N;++i) {
+//         scanf("%lld",&sta[i].money);
+//         minv = minv < sta[i].money ? minv : sta[i].money;
+//     }
+       
+//     for(long long i=next_sta+1;i<N;++i){
+//         ChargeDist = 0;
+//         if(sta[next_sta].money > sta[i].money) {
+//             for(long long j=next_sta;j<i;++j) ChargeDist += sta[j].dist;
+//             ans += (ChargeDist * sta[next_sta].money);
+//             next_sta = i;
+//         }else if( i == N-1 ){
+//             for(long long j=next_sta;j<i;++j) ChargeDist += sta[j].dist;
+//             ans += (ChargeDist * sta[next_sta].money);
+//         }
+//     }
+    
+//     printf("%lld\n",ans);
+//     return 0;
+// }
