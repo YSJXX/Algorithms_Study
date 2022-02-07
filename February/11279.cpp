de3c6817db4f9d9d158710229f80b5fe19f04a5e
Mac_YSJ <424tjdwns@gmail.com>
2022-02-07 21:27:12 +0900

    solved

--- February/11279.cpp
+++ February/11279.cpp
@@ -2,36 +2,32 @@
 
 int arr[100001]={0};
 int size=0;
-
 void swap(int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-void move(int index){
-    int parent = index/2;
-    while(parent >= 1){
-        if(arr[parent] > arr[index]) break;
-        swap(&arr[parent],&arr[index]);
-        index = parent;
-        parent = index/2;
-    }
+void move(int val){
+    arr[++size] = val; 
+    int idx = size;
+    while(idx != 1){
+        int parent = idx/2;
+        if(arr[parent] > arr[idx]) break;
+        swap(&arr[parent],&arr[idx]);
+        idx = parent;
+    }   
 }
 
-void del(){
-    int index = 1;
-    int left = index*2;
-    arr[index] = arr[size+1];
-    while(left <= size+1){
+void del(int index){
+    arr[1] = arr[size--];
+    while(index*2 <= size+1){
+        int left = index*2;
         int right = left+1;
-        if(right <= size+1)
-            if(arr[right] > arr[left]) left = right;
-        if(arr[left] < arr[index]) break;
-        swap(&arr[index],&arr[left]);
+        if(right <= size && arr[left] < arr[right]) left = right;
+        if(arr[index] > arr[left]) break;
+        swap(&arr[left],&arr[index]);
         index = left;
-        left = index*2;
     }
-    --size;
 }
 
 int main(){
@@ -44,14 +40,10 @@ int main(){
             if(size == 0) printf("0\n");
             else {
                 printf("%d\n",arr[1]); //첫번째 값이 제일 큰 값으로
-                del();
+                del(1);
             }
         }
-        else{
-            arr[size+1] = num;
-            move(size+1);
-            ++size;
-        }
+        else move(num);
     }
     return 0;
 }
