4bd9c3789636a551e6fa1212ecf8393d5bd34312
Mac_YSJ <424tjdwns@gmail.com>
2022-03-04 00:39:13 +0900

    Floyd-Warshall

---
+++ March/1956.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i = a; i < b; i++)
+void solve(){
+    int V=0,E;cin>>V>>E;
+    int d[V+1][V+1];
+    int ans=0x3f3f3f3f;
+    for(int i=0;i<=V;++i){
+        fill(d[i],d[i]+V+1,0x3f3f3f3f);
+    }
+    
+    for(int i=0;i<E;++i){
+        int a,b,c;cin>>a>>b>>c;
+        d[a][b]=c;
+    }
+
+    for(int k=1;k<=V;++k){
+        for(int i=1;i<=V;++i){
+            for(int j=1;j<=V;++j){
+                d[i][j] = min(d[i][j], d[i][k]+d[k][j]);
+            }
+        }
+    }
+    for(int i=1;i<=V;++i){
+        ans = min(ans,d[i][i]);
+    }
+    if(ans == 0x3f3f3f3f) ans=-1;
+    cout<<ans<<'\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();    
+    return 0;
+}
