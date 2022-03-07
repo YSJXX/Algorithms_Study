d5e38a96f4000f20e80342df3f7870f5b61eb64f
Mac_YSJ <424tjdwns@gmail.com>
2022-03-07 15:48:49 +0900

    math

---
+++ March/2775.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int F(int K, int N){
+    if(N==1) return 1;
+    if(K==0) return N;
+    return (F(K-1, N) + F(K, N-1));
+}
+
+void solve(){
+    int T;cin>>T;
+    while(T--){
+        int K,N;cin>>K>>N;
+        cout<<F(K,N)<'\n';
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();    
+    return 0;
+}
