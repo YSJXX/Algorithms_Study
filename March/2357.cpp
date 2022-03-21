5d2568a2ae1574a730d40c58363f58864dc351ae
Mac_YSJ <424tjdwns@gmail.com>
2022-03-21 22:57:34 +0900

    segment tree

--- March/2357.cpp
+++ March/2357.cpp
@@ -3,32 +3,27 @@ using namespace std;
 
 int N,M;
 vector<long long> arr(100001);
-void update(int st,int end){
-    vector<long long> MAX(N+1,0);
-    vector<long long> MIN(N+1,0xFFFFFFFF);
-    for(int idx=st;idx<=end;++idx){
-        int i = idx;
-        while(i<=N){
-            MAX[i] = MAX[i] > arr[idx] ? MAX[i] : arr[idx];
-            MIN[i] = MIN[i] < arr[idx] ? MIN[i] : arr[idx];
-            i += (i&-i);
-        }
-    }
+vector<long long> MAX(N+1,0);
+vector<long long> MIN(N+1,0xFFFFFFFF);
 
-    long long maxN=0;
-    long long minN=0xFFFFFFFF;
-    while(end>st){
-        maxN = max(maxN,MAX[end]);
-        minN = min(minN,MIN[end]);
-        --end;
+void update(int i,int val){
+    while(i<=N){
+        MAX[i] = MAX[i] > val ? MAX[i] : val;
+        MIN[i] = MIN[i] < val ? MIN[i] : val;
+        i += (i&-i);
     }
-    cout<<minN<<" "<<maxN<<'\n';
 }
 
+long long prefix(int i){
+    
+}
 void solve(){
     cin>>N>>M;
 
-    for(int i=1;i<=N;++i) cin>>arr[i];
+    for(int i=1;i<=N;++i) {
+        cin>>arr[i];
+        update(i,arr[i]);
+    }
     
     for(int i=0;i<M;++i){
         int a,b;cin>>a>>b;
