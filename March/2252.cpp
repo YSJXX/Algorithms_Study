2ae18c0a4397339ab7c1234c76de4c5140c87cf6
Mac_YSJ <424tjdwns@gmail.com>
2022-03-23 22:40:15 +0900

    위상정렬

---
+++ March/2252.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int N,M;cin>>N>>M;
+    
+    vector<int> ans;
+    queue<int> q;
+    vector<int> d(N+1,0);
+    vector<int> node[32001];
+    int A,B;
+    for(int i=0;i<M;++i){
+        cin>>A>>B;
+        ++d[B];
+        node[A].push_back(B);
+    }
+
+    for(int i=1;i<=N;++i){
+        if(d[i] == 0) q.push(i);
+    }
+
+    while(!q.empty()){
+        int cur = q.front(); q.pop();
+        cout << cur << ' ';
+        for(int nxt : node[cur]){
+            d[nxt]--;
+            if(d[nxt] == 0) q.push(nxt);
+        }
+    }
+    cout<<'\n';
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();    
+    return 0;
+}
