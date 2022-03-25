8dd956082bdb70aea886bdc2022ced5ac3c22308
Mac_YSJ <424tjdwns@gmail.com>
2022-03-25 23:54:59 +0900

    solved

---
+++ March/5337.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	cout << ".  .   ." << endl;
+	cout << "|  | _ | _. _ ._ _  _" << endl;
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();    
+    return 0;
+}
