e1225d7248d026bf2fe0323fa4a85b0cbe7c8e2a
Mac_YSJ <424tjdwns@gmail.com>
2022-04-02 04:27:00 +0900

    math

---
+++ April/5522.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int num=0;
+    int sum=0;
+    while(scanf("%d",&num) != -1)
+        sum+=num;
+
+    cout<<sum<<'\n';
+}
+
+int main(){
+    
+
+    solve();    
+    return 0;
+}
