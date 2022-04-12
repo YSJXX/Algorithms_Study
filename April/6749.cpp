1453f9c97985582d18d58e3fa80e752c253ba870
Mac_YSJ <424tjdwns@gmail.com>
2022-04-12 23:46:17 +0900

    solved

---
+++ April/6749.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int x, y;
+    cin>>x>>y;
+    cout << y + y - x<<'\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();    
+    return 0;
+}
