00e859ae43b0e402b235ce83a95ee2cd58abd5e1
Mac_YSJ <424tjdwns@gmail.com>
2022-05-16 01:28:16 +0900

    SW Expert Java 스탬프 찍

---
+++ May/SW_Ex_2046.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class SW_Ex_2046 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= N; test_case++)
+		{
+			System.out.print("#");
+		}
+
+	}
+
+}
